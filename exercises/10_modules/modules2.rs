016d3640af58b120700e8742707e8afe3bc4d27b
m1ckc3b <cebulski.mi@gmail.com>
2024-10-11 18:09:23 +0200

    done

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -1,18 +1,21 @@
 // You can bring module paths into scopes and provide new names for them with
 // the `use` and `as` keywords.
 
+use delicious_snacks::fruits::APPLE as fruit;
+use delicious_snacks::veggies::CARROT as veggie;
+
 #[allow(dead_code)]
 mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
     // use self::fruits::PEAR as ???;
     // use self::veggies::CUCUMBER as ???;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
@@ -21,7 +24,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
+        fruit,
+        veggie,
     );
 }
